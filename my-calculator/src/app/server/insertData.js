193869fc0f894284b6d9e8b3d256b55c5920147b
Alexis Navarro <navarroalexis007@gmail.com>
2025-03-28 16:20:33 -0600

    added sql files and connection to sql db

---
+++ my-calculator/src/app/server/insertData.js
@@ -0,0 +1,26 @@
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+
+
+const dbPath = path.join(__dirname, "../../app/database/calculator.db");
+console.log(`Using database path: ${dbPath}`);
+
+const db = new sqlite3.Database(dbPath, (err)=>{
+    if(err){
+        console.error("❌ Database Connection Error:", err.message);
+        return;
+    }
+    console.log("✅ Connected to SQLite database.");
+});
+
+const equation = "10/2";
+const result = "5";
+
+db.run("INSERT INTO history(equation, result) VALUES(?,?)",[equation,result],function(err){
+    if(err){
+        return console.error(err.message);
+    }
+    console.log(`New entry added with ID: ${this.lastID}`);
+});
+
+db.close();
